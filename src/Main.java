be4225f427f6a28cb3c8cb448b921ae5f40b585e
ericatkins8 <eric.atkins@hilliardschools.org>
2022-09-15 13:32:16 -0400

    finished project 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int intOperandA = 4;
+        int intOperandB = 3;
+        double doubleOperandA = 6;
+        double doubleOperandB = 8;
+
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " & " + intOperandB + " is " + intSum);
+
+        int intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " & " + intOperandB + " is " + intProduct);
+
+        int intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using ints of " + intOperandA + " & " + intOperandB + " is " + intDifference);
+
+        int intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of " + intOperandA + " & " + intOperandB + " is " + intQuotient);
+
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using ints of " + intOperandA + " & " + intOperandB + " is " + intModulo);
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " & " + doubleOperandB + " is " + doubleSum);
+
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " & " + doubleOperandB + " is " + doubleProduct);
+
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " & " + doubleOperandB + " is " + doubleDifference);
+
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " & " + doubleOperandB + " is " + doubleQuotient);
+
+        int kidsInFamily = 4;
+        boolean isRaining = false;
+        double gasPrice = 3.69;
+        int myFavoriteNumber = 8;
+        double shoeSize = 12.0;
+        int birthMonth = 10;
+        String fullName = "Eric Atkins";
+
+
+
+
     }
 }
